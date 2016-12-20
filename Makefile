30ffa9af8b49e95c105421ba85e4cd8fc5cac7b8
Jordan Dick <jchan1e@gmail.com>
2016-12-20 11:41:06 -0700

    fixed a bunch of issues

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # OSX
 ifeq "$(shell uname)" "Darwin"
-LFLAGS=-L/usr/local/Cellar/sfml/2.3/lib -L/usr/local/lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -lfftw3 -lm
-CFLAGS=-Wall -g -O3 -I/usr/local/Cellar/sfml/2.3/include/ -I/usr/local/include/
+LFLAGS=-L/usr/local/Cellar/sfml/2.4.0/lib -L/usr/local/lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -lfftw3 -lm
+CFLAGS=-Wall -g -O3 -I/usr/local/Cellar/sfml/2.4.0/include/ -I/usr/local/include/
 
 # Otherwise assume Linux
 else
--- soundwave.cpp
+++ soundwave.cpp
@@ -9,8 +9,8 @@
 
 using namespace std;
 
-const int W = 1920;   // window width
-const int S = 1080;   // window height
+int W = 1920;   // window width
+int S = 1080;   // window height
 const int Q = 1;   // bar width
 //const int N = W/Q;   // array size
 
@@ -32,7 +32,7 @@ public:
    virtual bool onStart()
    {
       changed = false;
-      setProcessingInterval(sf::milliseconds(17));
+      setProcessingInterval(sf::milliseconds(16));
       return true;
    }
 
@@ -83,34 +83,26 @@ void draw_wave(int n, int arr[]);
 void draw_array(int n, double arr[]);
 int pixel(double wx, double w0, double w1, int p0, int p1);
 
-sf::RenderWindow window(sf::VideoMode(W, S), "SoundWave");
+sf::RenderWindow* window;
 
 int main(int argc, char* argv[])
 {
    if (argc > 2)
    {
       // Cannot call main as a function.
-      //
+
       //char* argd[1];
       //for (int i = 1; i < argc; ++i)
       //{
       //   argd[0] = argv[i];
       //   main(2, argd);
       //}
+
       printf("Usage: ./soundwave [audiofile]\n");
       return 1;
    }
-   else if (argc < 2)
+   else if (argc < 2) //run on raw mic input
    {
-      sf::ContextSettings settings;
-      settings.antialiasingLevel = 0;
-      window.setVerticalSyncEnabled(true);
-
-      window.clear(sf::Color(0, 0, 0));
-      window.display();
-      window.clear(sf::Color(0, 0, 0));
-      window.display();
-
       myRec rec;
       if (!rec.isAvailable())
       {
@@ -119,12 +111,24 @@ int main(int argc, char* argv[])
       }
 
       vector<string> strang = rec.getAvailableDevices();
-      for (int i = strang.size()-1; i >= 0; i--)
+      for (int i = 0; i < strang.size(); ++i)
          cout << strang[i] << endl;
       //cout << rec.getDefaultDevice() << endl << rec.getDevice() << endl;
-      int choice;
-      cin >> choice;
+      int choice = 0;
+      if (strang.size() > 1)
+         cin >> choice;
       rec.setDevice(rec.getAvailableDevices()[choice]);
+
+      window = new sf::RenderWindow(sf::VideoMode(W, S), "SoundWave");
+
+      sf::ContextSettings settings;
+      settings.antialiasingLevel = 0;
+      window->setVerticalSyncEnabled(true);
+
+      window->clear(sf::Color(0, 0, 0));
+      window->display();
+      window->clear(sf::Color(0, 0, 0));
+      window->display();
 
       rec.start(88200);
 
@@ -132,11 +136,11 @@ int main(int argc, char* argv[])
       printf("SampleRate: %d\n", rate);
       //const sf::Int16* samples = buffer.getSamples();
 
-      int n = rate/12;
+      int n = rate/15;
       int f = W;
       int arr[n];
       double processed[n];
-      double display[f];
+      double* display = (double*)malloc(f*sizeof(double));
 
       //double factor = rate/1000.0;
 
@@ -156,20 +160,29 @@ int main(int argc, char* argv[])
       int i = 0;
 
       //rec.start();
-      while (window.isOpen())
+      while (window->isOpen())
       {
          sf::Event event;
-         while (window.pollEvent(event))
+         while (window->pollEvent(event))
          {
             if (event.type == sf::Event::Closed)
             {
                rec.stop();
-               window.close();
+               window->close();
                fftw_destroy_plan(plan);
                fftw_free(in);
                fftw_free(out);
 
                return EXIT_SUCCESS;
+            }
+            else if (event.type == sf::Event::Resized)
+            {
+               W = event.size.width;
+               S = event.size.height;
+               f = W;
+               free(display);
+               display = (double*)malloc(f*sizeof(double));
+               window->setView(sf::View(sf::FloatRect(0, 0, event.size.width, event.size.height)));
             }
          }
 
@@ -188,6 +201,153 @@ int main(int argc, char* argv[])
             in[i] = (double)arr[i];
          }
          //cout << "Current: " << current << "\nOffset: " << offset << "\t" << offset + n << endl;
+
+         fftw_execute(plan);
+
+         for (i = 0; i < n; ++i)
+         {
+            out[i][0] *= 2.0/n;
+            out[i][1] *= 2.0/n;
+            processed[i] = out[i][0]*out[i][0] + out[i][1]*out[i][1];
+            //processed[i] = powf(processed[i], 1.0/2.0)/60 * (i<pow(M_E, 3.0) ? (0.1*((float)i - pow(M_E, 3.0)) + 1) : log(pow((float)i, 1.0/3.0)));
+            processed[i] = powf(processed[i], 1.0/2.0)/60 * (i<pow(M_E, 3.0) ? 1 : log(pow((float)i, 1.0/3.0)));
+//            processed[i] = 10.0/log(10.0) * log(processed[i] + 1e-5);
+            if (processed[i] < 0.0)
+               processed[i] = 0.0;
+//            else if (processed[i] > 96.0)
+//               processed[i] = 96.0;
+         }
+
+         double A = 40.0/(rate/n);
+         double r = pow(20000.0/A*n/rate, 1.0/f);
+         // n = A*r^f
+         // r = (n/A)^(1/f)
+         // 20000 = 20*r^1920
+         int pre;
+         int nex;
+         for (i = 0; i < f; ++i)
+         {
+            pre = A*pow(r, i+1);
+            nex = A*pow(r, i+2);
+            if (nex == pre) ++nex;
+            display[i] = 0;
+            //cout << i << ",\t" << pre << endl;
+            for (int j = pre; j < nex; ++j)
+            {
+               if (j < n)
+                  display[i] = max(display[i], processed[j]);
+            }
+         }
+      //cout << f << endl;
+      //cout << n << endl;
+
+         window->clear(sf::Color(0, 0, 0));
+         
+         draw_array(f, display);
+         draw_wave(n, arr);
+         window->display();
+         //draw_array(f, display);
+         //draw_wave(n, arr);
+         //window->display();
+      }
+
+      free(display);
+      fftw_destroy_plan(plan);
+      fftw_free(in);
+      fftw_free(out);
+      delete window;
+
+      return EXIT_SUCCESS;
+   }
+   else //(argc == 2) Run on audio file
+   {
+
+//      else if (argc == 1)
+//         goto MIC;
+      srand(time(NULL));
+
+      //int array[N] = {0};
+      int i;
+
+      sf::ContextSettings settings;
+      settings.antialiasingLevel = 0;
+      window = new sf::RenderWindow(sf::VideoMode(W, S), "SoundWave");
+      window->setFramerateLimit(60);
+
+      sf::SoundBuffer buffer;
+      if (!buffer.loadFromFile(argv[1]))
+      {
+         printf("Could Not Load Audio File\n");
+         printf("%s\n", argv[1]);
+         return 1;
+      }
+      printf("%s\n", argv[1]);
+      sf::Sound sound;
+      sound.setBuffer(buffer);
+
+      int rate = buffer.getSampleRate();
+      printf("SampleRate: %d\n", rate);
+      const sf::Int16* samples = buffer.getSamples();
+
+      int n = rate/15;
+      int arr[n];
+      int f = W;
+      double processed[n];
+      double* display = (double*)malloc(f*sizeof(double));
+      int offset = 0;
+
+      double factor = rate/1000.0;
+
+      double *in;
+      fftw_complex *out;
+      fftw_plan plan;
+
+      in = (double*) fftw_malloc(sizeof(double) * n);
+      out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * n);
+      plan = fftw_plan_dft_r2c_1d(n, in, out, FFTW_PATIENT);
+
+      //for (i = 0; i < n; ++i)
+      //   arr[i] = (int)samples[i+offset];
+
+      sound.play();
+
+      sf::Event event;
+      while (window->isOpen())
+      {
+         while (window->pollEvent(event))
+         {
+            if (event.type == sf::Event::Closed)
+            {
+               window->close();
+               break;
+            }
+            else if (event.type == sf::Event::Resized)
+            {
+               W = event.size.width;
+               S = event.size.height;
+               f = W;
+               free(display);
+               display = (double*)malloc(f*sizeof(double));
+               window->setView(sf::View(sf::FloatRect(0, 0, event.size.width, event.size.height)));
+            }
+         }
+
+         if (sound.getStatus() != sf::Music::Playing)
+         {
+            window->close();
+            break;
+         }
+
+         offset = (int) (factor * sound.getPlayingOffset().asMilliseconds());
+         //printf("%d\n", offset);
+         for (i = 0; i < n; ++i)
+         {
+            if (i >= 0)
+               arr[i] = avg(buffer.getChannelCount(), samples, i + offset - n);
+            else
+               arr[i] = 0;
+            in[i] = (double)arr[i];
+         }
 
          fftw_execute(plan);
 
@@ -227,116 +387,18 @@ int main(int argc, char* argv[])
       //cout << f << endl;
       //cout << n << endl;
 
-         window.clear(sf::Color(0, 0, 0));
+         window->clear(sf::Color(0, 0, 0, 12));
          draw_array(f, display);
          draw_wave(n, arr);
-         window.display();
-         //draw_array(f, display);
-         //draw_wave(n, arr);
-         //window.display();
+         window->display();
       }
 
-   }
-   else //(argc == 2)
-   {
-
-//      else if (argc == 1)
-//         goto MIC;
-      srand(time(NULL));
-
-      //int array[N] = {0};
-      int i;
-
-      sf::ContextSettings settings;
-      settings.antialiasingLevel = 0;
-      window.setFramerateLimit(60);
-
-      sf::SoundBuffer buffer;
-      if (!buffer.loadFromFile(argv[1]))
-      {
-         printf("Could Not Load Audio File\n");
-         printf("%s\n", argv[1]);
-         return 1;
-      }
-      printf("%s\n", argv[1]);
-      sf::Sound sound;
-      sound.setBuffer(buffer);
-
-      int rate = buffer.getSampleRate();
-      printf("SampleRate: %d\n", rate);
-      const sf::Int16* samples = buffer.getSamples();
-
-      int n = rate/15;
-      int arr[n];
-      double processed[n];
-      int offset = 0;
-
-      double factor = rate/1000.0;
-
-      double *in;
-      fftw_complex *out;
-      fftw_plan plan;
-
-      in = (double*) fftw_malloc(sizeof(double) * n);
-      out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * n);
-      plan = fftw_plan_dft_r2c_1d(n, in, out, FFTW_PATIENT);
-
-      //for (i = 0; i < n; ++i)
-      //   arr[i] = (int)samples[i+offset];
-
-      sound.play();
-
-      sf::Event event;
-      while (window.isOpen())
-      {
-         while (window.pollEvent(event))
-         {
-            if (event.type == sf::Event::Closed)
-            {
-               window.close();
-               break;
-            }
-         }
-
-         if (sound.getStatus() != sf::Music::Playing)
-         {
-            window.close();
-            break;
-         }
-
-         offset = (int) (factor * sound.getPlayingOffset().asMilliseconds());
-         //printf("%d\n", offset);
-         for (i = 0; i < n; ++i)
-         {
-            arr[i] = avg(buffer.getChannelCount(), samples, i + offset);
-            in[i] = (double)arr[i];
-         }
-
-         fftw_execute(plan);
-
-         for (i = 0; i < n; ++i)
-         {
-            out[i][0] *= 2.0/n;
-            out[i][1] *= 2.0/n;
-            processed[i] = out[i][0]*out[i][0] + out[i][1]*out[i][1];
-            processed[i] = powf(processed[i], 1.0/2.5)/10;
-//            processed[i] = 10.0/log(10.0) * log(processed[i] + 1e-5);
-            if (processed[i] < 0.0)
-               processed[i] = 0.0;
-//            else if (processed[i] > 96.0)
-//               processed[i] = 96.0;
-         }
-
-         window.clear(sf::Color(0, 0, 0, 12));
-         draw_array(n, processed);
-         draw_wave(n, arr);
-         window.display();
-      }
-
+      free(display);
       fftw_destroy_plan(plan);
       fftw_free(in);
       fftw_free(out);
-  
+      delete window;
+
       return EXIT_SUCCESS;
    }
 }
@@ -358,39 +420,43 @@ void draw_wave(int n, int arr[])
 {
    int i; //y;
 
-   sf::Vertex line0[] =
-   {  
-      sf::Vertex(sf::Vector2f(0, 0)),
-      sf::Vertex(sf::Vector2f(0, 0)),
-   };
+   sf::Vertex* line0 = (sf::Vertex*)malloc(2*n*sizeof(sf::Vertex));
+   //{  
+   //   sf::Vertex(sf::Vector2f(0, 0)),
+   //   sf::Vertex(sf::Vector2f(0, 0)),
+   //};
 
-   int u = 98304 / S;
+   //int u = 98304 / S;
+   float u = (S/3.0)/(256.0*128.0);
    for(i = 1; i < n; ++i)
    {
-      line0[0] = sf::Vertex(sf::Vector2f(2*(i-1), 2*S/3-(arr[i-1]/u)));
-      line0[1] = sf::Vertex(sf::Vector2f(2*i, 2*S/3-(arr[i]/u)));
+      //line0[2*i] = sf::Vertex(sf::Vector2f(2*(i-1), S-S/3.0));
+      //line0[2*i+1] = sf::Vertex(sf::Vector2f(2*i, S-S/3.0));
+      line0[2*i] = sf::Vertex(sf::Vector2f(2*(i-1), S-S/3.0-(arr[i-1]*u)));
+      line0[2*i+1] = sf::Vertex(sf::Vector2f(2*i, S-S/3.0-(arr[i]*u)));
    //   line0[1].color = sf::Color::Color (
    //      min(abs(max(abs(((2*arr[i]+512) %1536)-768)-256,0)),255),
    //      min(abs(max(abs(((2*arr[i]) %1536)-768)-256,0)),255),
    //      min(abs(max(abs(((2*arr[i]-512) %1536)-768)-256,0)),255));
       line0[1].color = sf::Color::White;
-      line0[0].color = line0[1].color;
+      line0[0].color = sf::Color::White;
 //      line0[0].color = sf::Color::Black;
-      window.draw(line0, 2, sf::Lines);//Strip);
    }
+   window->draw(line0, 2*n, sf::Lines);//Strip);
+   free(line0);
 }
 
 void draw_array(int n, double arr[])
 {
    int i; //y;
 
-   sf::Vertex line0[] =
-   {  
-      sf::Vertex(sf::Vector2f(0, 0)),
-      sf::Vertex(sf::Vector2f(0, 0)),
-   };
+   sf::Vertex line0[4];
+   //{  
+   //   sf::Vertex(sf::Vector2f(0, 0)),
+   //   sf::Vertex(sf::Vector2f(0, 0)),
+   //};
 
-   float u = S/300;
+   float u = S/300.0;
 
    for(i = 1; i < n; ++i)
    {
@@ -398,22 +464,27 @@ void draw_array(int n, double arr[])
       //   printf("%f ", arr[i]);
       for(int j = 0; j < Q; ++j)
       {
-         line0[0] = sf::Vertex(sf::Vector2f(Q*i+j, S/3-(arr[i]*u)));
-         line0[1] = sf::Vertex(sf::Vector2f(Q*i+j, S/3+(arr[i]*u/2)));
+         line0[0] = sf::Vertex(sf::Vector2f(Q*i+j, S/3.0-(arr[i]*u)));
+         line0[1] = sf::Vertex(sf::Vector2f(Q*i+j, S/3.0));
+         line0[2] = sf::Vertex(sf::Vector2f(Q*i+j, S/3.0));
+         line0[3] = sf::Vertex(sf::Vector2f(Q*i+j, S/3.0+(arr[i]*u/2)));
          line0[1].color = sf::Color (
             min(abs(max(abs(((2*i/3) %1536)-768)-256,0)),255)*max(min(arr[i]/24.0, 1.0), 0.20),
             min(abs(max(abs(((2*i/3+1024) %1536)-768)-256,0)),255)*max(min(arr[i]/24.0, 1.0), 0.20),
             min(abs(max(abs(((2*i/3+512) %1536)-768)-256,0)),255)*max(min(arr[i]/24.0, 1.0), 0.20));
    //      line0[1].color = sf::Color::White;
-         line0[0].color = line0[1].color;
-//         line0[0].color = sf::Color::Black;
-         window.draw(line0, 2, sf::Lines);//Strip);
+//         line0[0].color = line0[1].color;
+         line0[2].color = line0[1].color;;
+         line0[0].color = line0[1].color;;
+         line0[3].color = sf::Color::Black;
+         //window->draw(line0, 2, sf::Lines);//Strip);
          //line0[0] = sf::Vertex(sf::Vector2f(2*Q*i+Q+j, S/3-(arr[i]*u)));
          //line0[0].color = line0[1].color;
          //line0[1] = sf::Vertex(sf::Vector2f(2*Q*i+Q+j, S/3+(arr[i]*u/2)));
          //line0[1].color = line0[0].color;
-         //window.draw(line0, 2, sf::Lines);
+         window->draw(line0, 4, sf::Lines);
       }
+      //window->draw(line0, 2*n, sf::Lines);//Strip);
    }
 }
 
